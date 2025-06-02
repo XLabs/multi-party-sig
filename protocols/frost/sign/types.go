63f8ab09f94fc9c9082f28f50762890e95fad10a
Jonathan Weiss <jonathan.weiss1@mail.huji.ac.il>
2025-06-02 10:06:39 +0300

    made the eth one public

--- protocols/frost/sign/types.go
+++ protocols/frost/sign/types.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 
 	"github.com/cronokirby/saferith"
-	"github.com/xlabs/multi-party-sig/internal/eth"
+	"github.com/xlabs/multi-party-sig/pkg/eth"
 	"github.com/xlabs/multi-party-sig/pkg/math/curve"
 	"github.com/xlabs/multi-party-sig/pkg/math/sample"
 
